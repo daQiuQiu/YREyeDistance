76b25d6c2854faa2db6edfaefc6ea8064ed3c002
yiren <ren_yi92@sina.com>
2021-08-23 22:44:47 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-YREyeDistance_Example/Pods-YREyeDistance_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YREyeDistance_Example : NSObject
+@end
+@implementation PodsDummy_Pods_YREyeDistance_Example
+@end
---
+++ Example/Pods/Target Support Files/YREyeDistance/YREyeDistance-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YREyeDistance : NSObject
+@end
+@implementation PodsDummy_YREyeDistance
+@end
